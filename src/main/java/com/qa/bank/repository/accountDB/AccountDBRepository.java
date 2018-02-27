873aa3762508800789fa6f6a93dfce677767f7ad
eurokennedy <eurokennedy@gmail.com>
2018-02-27 12:50:26 +0000

    get information from db from api

--- src/main/java/com/qa/bank/repository/accountDB/AccountDBRepository.java
+++ src/main/java/com/qa/bank/repository/accountDB/AccountDBRepository.java
@@ -8,6 +8,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import com.qa.domain.Account;
+import com.qa.domain.Customers;
 import com.qa.util.JSONUtil;
 
 public class AccountDBRepository implements AccountDBInterface{
@@ -17,33 +18,35 @@ public class AccountDBRepository implements AccountDBInterface{
 	
 	@Inject 
 	private JSONUtil jsonUtil;
-
+	
+	private static final String ACCOUNT_ACCOUNTNO_COLUMN = "SELECT ACCOUNTNO FROM ACCOUNT WHERE ACCOUNTID = ";
+    private static final String ACCOUNT_GETBALANCE_COLUMN = "SELECT BALANCE FROM ACCOUNT WHERE ACCOUNTID = ";
+    private static final String ACCOUNT_GETALL = "SELECT * FROM ACCOUNT WHERE ACCOUNTID = ";
+    private Query query;
+    
 	@Override
-	public String getAccountID() {
-		Query query = entityManager.createQuery("Select a FROM Account a");
-		Collection<Account> accounts = (Collection<Account>) query.getResultList();
-		return jsonUtil.getJSONForObject(accounts);
+	public String getAccountNo(String accountID) {
+		query = entityManager.createQuery(ACCOUNT_ACCOUNTNO_COLUMN + "'" + accountID + "'");
+	    Collection<Account> accountCollection = (Collection<Account>) query.getResultList();
+	    return jsonUtil.getJSONForObject(accountCollection);
+	}
+	@Override
+	public String getBalance(String accountID) {
+		query = entityManager.createQuery(ACCOUNT_GETBALANCE_COLUMN + "'" + accountID + "'");
+	    Collection<Account> accountCollection = (Collection<Account>) query.getResultList();
+	    return jsonUtil.getJSONForObject(accountCollection);
+	}    
+	@Override
+	public String getAccountAll(String accountID) {
+		query = entityManager.createQuery(ACCOUNT_GETALL + "'" + accountID + "'");
+	    Collection<Account> accountCollection = (Collection<Account>) query.getResultList();
+	    return jsonUtil.getJSONForObject(accountCollection);
+	}	
+	public void setManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
 	}
 
-	@Override
-	public String getAccountNo(String accountNo) {
-		Query query = entityManager.createQuery("Select a FROM Account a");
-		Collection<Account> accounts = (Collection<Account>) query.getResultList();
-		return jsonUtil.getJSONForObject(accounts);
+	public void setUtil(JSONUtil jsonUtil) {
+		this.jsonUtil = jsonUtil;
 	}
-
-	@Override
-	public float getBalance() {
-		Query query = entityManager.createQuery("Select a FROM Account a");
-		Collection<Account> accounts = (Collection<Account>) query.getResultList();
-		return 5.00F;
-	}
-
-	@Override
-	public String getCustomerID(String customerID) {
-		Query query = entityManager.createQuery("Select a FROM Account a");
-		Collection<Account> accounts = (Collection<Account>) query.getResultList();
-		return jsonUtil.getJSONForObject(accounts);
-	}
-
 }
