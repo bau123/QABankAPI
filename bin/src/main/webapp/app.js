9bf9556fe2f9f563e93a21b113b72a6a977794a7
bau123 <bauvannguyen123@gmail.com>
2018-02-27 17:18:30 +0000

    Create a method that gets the average amount a user spends over a time period

--- bin/src/main/webapp/app.js
+++ bin/src/main/webapp/app.js
@@ -1,23 +1,64 @@
 var app=angular.module('mcm', []); //'app-directives'
 
-app.controller("TransactionController", function($log)
+app.controller("TransactionController", function($log, $http)
 {
-	var allTransactions = [];
+	this.accountNumber;
+	this.allTransactions = [];
+	this.toDate = "march";
+	this.fromDate = "jan";
 	
-	this.storeTransactions = function()
-	{
-		$log.log("here");
-		 
-		var transaction = {
-			date: "test",
-			reference: "test",
-			_in: "test",
-			out: "test",
-			balance: "test",
-			type: "test"
-		}		
-		allTransactions.push(transaction);		
-		$log.log(this.allTransactions);
+	this.getTransactionsFromServer = function()
+	{  
+	    var _this = this;
+		$http.get('/london.api/rest/transaction/All/' + this.accountNumber + '/' + this.fromDate + "/" + this.toDate).then(function(response){
+			$log.log(response.data);			
+			_this.allTransactions = response.data;
+			
+			
+			for(var x = 0; x < _this.allTransactions.length; x++){
+				_this.allTransactions[x].dateOfTransaction = _this.allTransactions[x].dateOfTransaction.substr(0,12);
+			}
+		});	
+	
 	}
 	
+
+	
 });
+
+
+
+app.controller("AverageController", function($log, $http)
+{
+	this.accountNumber;
+	this.fromDate;
+	this.toDate;
+	this.avgSpending;
+	this.avgIncome;
+	
+	this.getAverageInfo = function()
+	{
+		$log.log("from: " + this.fromDate);
+		$log.log("to: " + this.toDate);
+		$log.log("getAverageInfo method");	    
+	    var _this = this;
+		$http.get('/london.api/rest/transaction/average/' + this.accountNumber + '/' + this.fromDate + "/" + this.toDate).then(function(response){
+			$log.log(response.data);
+			_this.fromDate = response.data.fromDate;
+			_this.toDate = response.data.toDate;
+		});	
+	}
+	
+	
+});
+
+
+
+
+
+
+
+
+
+
+
