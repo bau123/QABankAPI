512dcac2caec53f01c2358985bf3e9f7a86f4ad8
IsmaelOmar <omariy.ismael@gmail.com>
2018-02-27 12:57:38 +0000

    Changed some datatypes in the date based to make it easier to use GET methods using Postman

--- src/main/java/com/qa/domain/Transactions.java
+++ src/main/java/com/qa/domain/Transactions.java
@@ -12,25 +12,44 @@ import javax.persistence.JoinColumn;
 @Entity
 public class Transactions {
 	
-	@Id
-	private String TransactionID;
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String transactionID;
 	
 	@Column(columnDefinition="Decimal(10,2)")
 	private double amount;
 	
-	private String TransactionType;
+	private String transactionType;
 	
-	private String Description;
+	private String description;
 	
-	private String dateOfTransaction;
+	private Date dateOfTransaction;
 	
-	private String Deposit_Withdrawal;
+	private String deposit_Withdrawal;
 	
 	@Column(columnDefinition="Decimal(10,2)")
 	private double updatedBalance;
 	
 	@JoinColumn(name ="AccountID")
-	private String AccountID;
+	private String accountID;
+	
+	
+	public Transactions(){
+		
+	}
+	
+	public Transactions(String accountID, String deposit_Withdrawal,
+						String description, String transactionType, double amount, Date dateOfTransaction, 
+						double udpatedBalance){
+		
+			this.accountID = accountID;
+			this.deposit_Withdrawal = deposit_Withdrawal;
+			this.description = description;
+			this.transactionType = transactionType;
+			this.amount = amount;
+			this.dateOfTransaction = dateOfTransaction;
+			this.updatedBalance = updatedBalance;
+		
+	}
 	
 	
 }
